a44af92d6ef5cb17a7e1493614fd6cbabba8fd10
Charles Wu <charles.wu@reece.com.au>
2021-02-03 22:21:27 +1100

    spring boot init

---
+++ src/main/java/au/com/charleswu/wechatbot/WechatbotApplication.java
@@ -0,0 +1,13 @@
+package au.com.charleswu.wechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/charleswu/wechatbot/WechatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.charleswu.wechatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
